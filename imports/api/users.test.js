b241aced2e01bafc40e762ba8b4cefdae0ac3faa
Julian Currie <julian.currie@gmail.com>
2017-04-18 19:27:37 +0700

    Added users test that only runs on server

---
+++ imports/api/users.test.js
@@ -0,0 +1,21 @@
+import expect from 'expect';
+import { Meteor } from "meteor/meteor";
+import { validateNewUser } from '../../imports/api/users';
+
+if (Meteor.isServer) {
+  describe('users', () => {
+    it('should allow valid email address', () => {
+      const testUser = {
+        emails: [
+          {
+            address: 'julian.currie@gmail.com'
+          }
+        ]
+      };
+
+      const result = validateNewUser(testUser);
+
+      expect(result).toBe(true);
+    });
+  });
+}
