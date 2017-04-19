3255ca5a255da2f7aa07509eeb743e9346d8b234
Julian Currie <julian.currie@gmail.com>
2017-04-19 19:09:38 +0700

    Added notest update Meteor method w/ tests, moved out initial note

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -32,5 +32,35 @@ Meteor.methods({
 
     //The 2nd paramter insures that a user can't delete someone else's notes
     Notes.remove({ _id, userId: this.userId });
+  },
+  'notes.update'(_id, updates) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      },
+      title: {
+        type: String,
+        optional: true
+      },
+      body: {
+        type: String,
+        optional: true
+      }
+    }).validate({
+      _id,
+      ...updates
+    });
+
+    Notes.update(_id, {
+      $set: {
+        updatedAt: moment().valueOf(),
+        ...updates
+      }
+    });
   }
 });
