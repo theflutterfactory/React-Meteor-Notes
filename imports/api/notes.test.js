a5dbfe1e3ff2ec61bcd2843b0290723b344eb418
Julian Currie <julian.currie@gmail.com>
2017-04-19 19:09:54 +0700

    missing file in commit

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,15 +5,17 @@ import { Notes } from './notes';
 if (Meteor.isServer) {
   describe('notes', function () {
 
+    const noteOne = {
+      _id: 'testNoteId',
+      title: 'testTitle',
+      body: 'testBody',
+      updatedAt: 0,
+      userId: 'testUserId'
+    }
+
     beforeEach(() => {
       Notes.remove({});
-      Notes.insert({
-        _id: 'testNoteId',
-        title: 'testTitle',
-        body: 'testBody',
-        updatedAt: 0,
-        userId: 'testUserId'
-      })
+      Notes.insert(noteOne)
     });
 
     it('should insert a new note', function () {
@@ -31,20 +33,45 @@ if (Meteor.isServer) {
     });
 
     it('should remove note', () => {
-      Meteor.server.method_handlers['notes.remove'].apply({ 'userId': 'testUserId' }, ['testNoteId']);
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId }, [noteOne._id]);
 
-      expect(Notes.findOne({ _id: 'testNoteId' })).toNotExist();
+      expect(Notes.findOne({ _id: noteOne._id })).toNotExist();
     });
 
     it('should not remove note if unathenticated', () => {
       expect(() => {
-        Meteor.server.method_handlers['notes.remove'].apply({}, ['testNoteId']);
+        Meteor.server.method_handlers['notes.remove'].apply({}, [noteOne._id]);
       }).toThrow();
     });
 
     it('should not remove note with invalid note id', () => {
       expect(() => {
-        Meteor.server.method_handlers['notes.remove'].apply({ 'userId': 'testUserId' });
+        Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId });
+      }).toThrow();
+    });
+
+    it('should update note', () => {
+      const title = "Updated Title";
+      Meteor.server.method_handlers['notes.update'].apply({
+        userId: noteOne.userId
+      }, [noteOne._id, { title }]);
+
+      const note = Notes.findOne(noteOne._id);
+
+      expect(note.updatedAt).toBeGreaterThan(0);
+      expect(note).toInclude({
+        title,
+        body: noteOne.body
+      });
+    });
+
+    it('should throw error if note updates with unexpected extras', () => {
+      const title = "Updated Title";
+
+      expect(() => {
+        Meteor.server.method_handlers['notes.update'].apply({
+          userId: "noteOne.userId"
+        }, [noteOne._id, { title, extraKey: "Extra" }]);
       }).toThrow();
     });
   });
