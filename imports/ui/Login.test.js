207f6fead0bff5ca268ff2ab645199e14027daad
Julian Currie <julian.currie@gmail.com>
2017-04-23 19:01:28 +0700

    added login tests for form and error msg validation

--- imports/ui/Login.test.js
+++ imports/ui/Login.test.js
@@ -12,11 +12,37 @@ if (Meteor.isClient) {
       const wrapper = mount(<Login loginWithPassword={() => { }} />);
 
       wrapper.setState({ error });
-      const errorText = wrapper.find('p').text();
-      expect(errorText).toBe(error);
+      expect(wrapper.find('p').text()).toBe(error);
 
       wrapper.setState({ error: '' });
       expect(wrapper.find('p').length).toBe(0);
+    });
+
+    it('should call login method with form data', () => {
+      const email = 'julian@test.com';
+      const password = 'password123'
+      const spy = expect.createSpy();
+      const wrapper = mount(<Login loginWithPassword={spy} />)
+
+      wrapper.ref('emailRef').node.value = email;
+      wrapper.ref('passwordRef').node.value = password;
+      wrapper.find('form').simulate('submit');
+
+      expect(spy.calls[0].arguments[0]).toEqual({ email });
+      expect(spy.calls[0].arguments[1]).toBe(password);
+    });
+
+    it('should set login callback errors', () => {
+      const spy = expect.createSpy();
+      const wrapper = mount(<Login loginWithPassword={spy} />)
+
+      wrapper.find('form').simulate('submit');
+
+      spy.calls[0].arguments[2]({});
+      expect(wrapper.state('error').length).toNotBe('');
+
+      spy.calls[0].arguments[2]();
+      expect(wrapper.state('error').length).toBe(0);
     });
   });
 }
