6d617c1259fa8ffc6805a186b51635f566127f9b
Julian Currie <julian.currie@gmail.com>
2017-04-24 16:47:18 +0700

    new NoteList & NoteListHeader components, increment note count on btn click

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -1,12 +1,15 @@
 import React from "react";
 import PrivateHeader from "./PrivateHeader";
+import NoteList from './NoteList';
+import NoteListHeader from './NoteListHeader';
 
 export default () => {
   return (
     <div>
       <PrivateHeader title="Dashboard" />
       <div className="page-content">
-        Dashboard Page Content
+        <NoteListHeader />
+        <NoteList />
       </div>
     </div>
   );
---
+++ imports/ui/NoteList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { createContainer } from "meteor/react-meteor-data";
+import { Notes } from "../api/notes";
+
+export const NoteList = (props) => {
+  return (
+    <div>
+      NoteList {props.notes.length}
+    </div>
+  )
+};
+
+NoteList.propTypes = {
+  notes: React.PropTypes.array.isRequired
+};
+
+export default createContainer(() => {
+  Meteor.subscribe('notes');
+
+  return {
+    notes: Notes.find().fetch()
+  }
+}, NoteList);
