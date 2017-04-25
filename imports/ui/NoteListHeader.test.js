52bdd03674eef5f6df59088ba99c5aef3b5397e4
Julian Currie <julian.currie@gmail.com>
2017-04-25 15:56:03 +0700

    Added test for NoteListHeader button click

---
+++ imports/ui/NoteListHeader.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+import { Meteor } from 'meteor/meteor';
+import { NoteListHeader } from './NoteListHeader';
+
+if (Meteor.isClient) {
+  describe('NoteListHeader', () => {
+    it('should call meteorCall on click', () => {
+      const spy = expect.createSpy();
+      const wrapper = mount(<NoteListHeader meteorCall={spy} />)
+
+      wrapper.find('button').simulate('click');
+      expect(spy).toHaveBeenCalledWith('notes.insert');
+    });
+  });
+}
