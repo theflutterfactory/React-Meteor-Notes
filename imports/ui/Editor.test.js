48bcf009bf8242db88701a3b9a74f642811be249
Julian Currie <julian.currie@gmail.com>
2017-04-29 17:28:21 +0700

    added Editor tests for setting the state

--- imports/ui/Editor.test.js
+++ imports/ui/Editor.test.js
@@ -35,5 +35,46 @@ if (Meteor.isClient) {
       expect(browserHistory.push).toHaveBeenCalledWith('/dashboard');
       expect(call).toHaveBeenCalledWith('notes.remove', notes[0]._id);
     });
+
+    it('should update the note body on text area change', () => {
+      const wrapper = mount(<Editor browserHistory={browserHistory} call={call} selectedNoteId={notes[0]} note={notes[0]} />);
+      const newBody = 'This is the new body text';
+
+      wrapper.find('textarea').simulate('change', {
+        target: {
+          value: newBody
+        }
+      });
+
+      expect(wrapper.state('body')).toBe(newBody);
+      expect(call).toHaveBeenCalledWith('notes.update', notes[0]._id, { body: newBody });
+    });
+
+    it('should update the note title on input change', () => {
+      const wrapper = mount(<Editor browserHistory={browserHistory} call={call} selectedNoteId={notes[0]} note={notes[0]} />);
+      const newTitle = 'This is the new title';
+
+      wrapper.find('input').simulate('change', {
+        target: {
+          value: newTitle
+        }
+      });
+
+      expect(wrapper.state('title')).toBe(newTitle);
+      expect(call).toHaveBeenCalledWith('notes.update', notes[0]._id, { title: newTitle });
+    });
+
+    it('should set state for new note', () => {
+      const wrapper = mount(<Editor browserHistory={browserHistory} call={call} />);
+
+      wrapper.setProps({
+        selectedNoteId: notes[0]._id,
+        note: notes[0]
+      });
+
+      expect(wrapper.state('title')).toBe(notes[0].title);
+      expect(wrapper.state('body')).toBe(notes[0].body);
+
+    });
   });
 }
