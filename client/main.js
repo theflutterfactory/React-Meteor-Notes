ce3efe07c427d5990139bda5e884357abb30cf70
Julian Currie <julian.currie@gmail.com>
2017-05-01 00:11:30 +0700

    added new header images and isNavOpen Session variable

--- client/main.js
+++ client/main.js
@@ -22,5 +22,6 @@ Tracker.autorun(() => {
 
 Meteor.startup(() => {
   Session.set('selectedNoteId', undefined);
+  Session.set('isNavOpen', false);
   ReactDOM.render(routes, document.getElementById('app'));
 });
